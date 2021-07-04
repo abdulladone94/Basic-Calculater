ce57f01ae7819b81c3e4af0efe6706bfddfc5c92
abdulladone94 <abdulladone94@gmail.com>
2021-07-04 09:17:13 +0530

    one input field and useing eval

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
 var ad = document.getElementById("add");
-var sub = document.getElementById("subtract");
-var mul = document.getElementById("multiply");
-var div = document.getElementById("divide");
+var eq = document.getElementById("equal");
+// var sub = document.getElementById("subtract");
+// var mul = document.getElementById("multiply");
+// var div = document.getElementById("divide");
 
 ad.addEventListener("click", () => {
   handlePlus();
 });
-sub.addEventListener("click", () => {
-  handleMinus();
+eq.addEventListener("click", () => {
+  handleEqual();
 });
-mul.addEventListener("click", () => {
-  handleMultiply();
-});
-div.addEventListener("click", () => {
-  handleDivide();
-});
+// sub.addEventListener("click", () => {
+//   handleMinus();
+// });
+// mul.addEventListener("click", () => {
+//   handleMultiply();
+// });
+// div.addEventListener("click", () => {
+//   handleDivide();
+// });
 
 const handlePlus = (y, z) => {
   var y = document.getElementById("txt1").value;
-  var z = document.getElementById("txt2").value;
-  var x = +y + +z;
-  document.getElementById("ans").value = x;
-  console.log(x);
+  document.getElementById("txt1").value = y + "+";
 };
-const handleMinus = (y, z) => {
-  var y = document.getElementById("txt1").value;
-  var z = document.getElementById("txt2").value;
-  var x = +y - +z;
-  document.getElementById("ans").value = x;
-  console.log(x);
+
+const handleEqual = (y, z) => {
+  var z = window.eval(document.getElementById("txt1").value);
+  document.getElementById("txt1").value = z;
 };
-const handleMultiply = (y, z) => {
-  var y = document.getElementById("txt1").value;
-  var z = document.getElementById("txt2").value;
-  var x = +y * +z;
-  document.getElementById("ans").value = x;
-  console.log(x);
-};
-const handleDivide = (y, z) => {
-  var y = document.getElementById("txt1").value;
-  var z = document.getElementById("txt2").value;
-  var x = +y / +z;
-  document.getElementById("ans").value = x;
-  console.log(x);
-};
+// const handleMinus = (y, z) => {
+//   var y = document.getElementById("txt1").value;
+//   var z = document.getElementById("txt2").value;
+//   var x = +y - +z;
+//   document.getElementById("ans").value = x;
+//   console.log(x);
+// };
+// const handleMultiply = (y, z) => {
+//   var y = document.getElementById("txt1").value;
+//   var z = document.getElementById("txt2").value;
+//   var x = +y * +z;
+//   document.getElementById("ans").value = x;
+//   console.log(x);
+// };
+// const handleDivide = (y, z) => {
+//   var y = document.getElementById("txt1").value;
+//   var z = document.getElementById("txt2").value;
+//   var x = +y / +z;
+//   document.getElementById("ans").value = x;
+//   console.log(x);
+// };
